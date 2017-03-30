271b3cd444541ee06fd1b4629746640dfc4a973c
zjhch123 <416836508@qq.com>
2017-03-30 22:00:09 +0800

    first

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: "./entry.js",
+    output: {
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: "/node_modules/",
+                loader: "babel-loader"
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.common.js'
+        }
+    }
+}
